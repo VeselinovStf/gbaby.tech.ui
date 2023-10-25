9dd2e26b7a4ca070ba8635fc3c371aeca031dc11
Stefan Veselinov <chofexx@gmail.com>
2023-10-25 20:31:08 +0300

    Initial Comming Soon UI

---
+++ gbaby_tech_ui_v01/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'gbaby_tech_ui_v01';
+}
